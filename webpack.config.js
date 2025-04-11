ff0989f40fc72efbc41dc8a4f8db99ff2860be27
Tatiana <tvtugova1511@gmail.com>
2025-04-11 11:38:02 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+module.exports = {
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "src/index.html", to: "." }],
+    }),
+  ],
+};
